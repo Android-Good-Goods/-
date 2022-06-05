bcab2a784ce4239ca1da0f488f0b0b5a695d31dc
Cjc-001214 <1874534244@qq.com>
2022-06-05 22:29:34 +0800

    功能修改

--- 后端/HaoWuApplication_Springboot/src/main/java/net/Implementist/service/UserService.java
+++ 后端/HaoWuApplication_Springboot/src/main/java/net/Implementist/service/UserService.java
@@ -9,20 +9,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  *  服务类
  * </p>
  *
- * @author 
+ * @author
  * @since 2022-05-21
  */
 public interface UserService extends IService<User> {
-
-    User queryUserByAccount(String account);
-
-    JSONObject signupVerify(String account, String pwd, String nickName, String tel, int sex);
-
-    JSONObject Loginverify(String account,String password);
-
-    void updateUser(User user);
-
-    boolean refreshUserByAccount(User user) throws Exception;
-
-    JSONObject ModifyPwd(String account,String password,String newpassword);
 }
